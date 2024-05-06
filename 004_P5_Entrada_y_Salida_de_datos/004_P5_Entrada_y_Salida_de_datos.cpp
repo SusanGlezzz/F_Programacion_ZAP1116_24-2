3a6b7bcd23ba2e964ead5c13712fdd32181c41dd
SusanGlezzz <susanacasillas1411@gmail.com>
2024-05-06 09:03:30 -0600

    004_susy
    crear la carpeta

---
+++ 004_P5_Entrada_y_Salida_de_datos/004_P5_Entrada_y_Salida_de_datos.cpp
@@ -0,0 +1,11 @@
+// 004_P5_Entrada_y_Salida_de_datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// 
+
+#include <iostream>
+
+int main()
+{
+   
+}
+
+
